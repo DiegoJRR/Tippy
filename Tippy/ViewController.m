ec81ecc9c2269c962274da76766fdbb89f209d43
Diego de Jesus Ramirez <diegojrr@fb.com>
2020-06-23 18:47:04 -0500

    Added basic tipping calculation functionality, tip percentage options and display of calculated values

--- Tippy/ViewController.m
+++ Tippy/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *mealBillField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
 
 @end
 
@@ -19,5 +23,24 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)onTap:(id)sender {
+    // Calling the endEditing method with argument YES on
+    //the self.view object
+    [self.view endEditing:YES];
+}
+
+- (IBAction)onEdit:(id)sender {
+    double bill = [self.mealBillField.text doubleValue];
+    
+    NSArray *percentages = @[@(0.1), @(0.2), @(0.3)];
+    
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    
+    double tip = tipPercentage * bill;
+    double total = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+}
 
 @end
